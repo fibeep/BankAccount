5945ab0a12308004ff164772a429de0adef95ef7
Salo <salomonco@gmail.com>
2020-10-30 14:03:23 -0400

    deposit function works

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,20 @@
+from random import randint
+
 class BankAccount:
-    def __innit__(self, full_name, account_number, routing_number, balance):
+    balance = 0.00
+    routing_number = 123456789
+    account_number = randint(100000000,999999999)
+    """ Initializes Bank Account with Attributes """
+    def __innit__(self, full_name, account_number):
         self.full_name = full_name
-        self.account_number = account_number
-        self.routing_number = routing_number
-        self.balance = balance
+        
+    
+    """ Adds a deposit function to edit the balance """
+    def deposit(self, amount):
+        self.balance += amount
+        print(f'Amount Deposited: ${amount}')
+        print(f'New Balance: ${self.balance}')
+
+salo = BankAccount()
+salo.deposit(3)
+print(salo.balance)
